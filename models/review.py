e5beaa3a6ad0c8f7a77c4cfb5eacd8d729ff277b
Oscar Riaño Tapias <oscarnetworkingpro@gmail.com>
2020-03-31 16:52:30 -0500

    update review

--- models/review.py
+++ models/review.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is the review class"""
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
 from os import getenv
 
@@ -12,13 +13,12 @@ class Review(BaseModel, Base):
         user_id: user id
         text: review description
     """
-
+    __tablename__ = "reviews"
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = "reviews"
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-
-    place_id = ""
-    user_id = ""
-    text = ""
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
