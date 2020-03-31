2e3df7fb7d2cf48cbbf053ab5a12279c602b9cf3
OscarDRT <55116114+OscarDRT@users.noreply.github.com>
2020-03-31 10:31:36 -0500

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from os import environ
-from sqlalchemy.orm import relationship
 import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-class City(BaseModel):
+
+class City(BaseModel, Base):
     """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
-    __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    place = relationship("Place", backref="cities", cascade="all, delete" )
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+    else:
+        state_id = ""
+        name = ""
