cafa4a1f6f38f6f64c4fa165ecbb9b458f007157
Andresmelek <candres.isaza@gmail.com>
2020-03-31 14:20:41 -0500

    Updating class user

--- models/user.py
+++ models/user.py
@@ -13,14 +13,8 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
