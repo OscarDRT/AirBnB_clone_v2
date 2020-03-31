4dc507c6b4b93dc0ec87395194931a9229766f9f
OscarDRT <55116114+OscarDRT@users.noreply.github.com>
2020-03-31 10:32:26 -0500

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from os import environ
-from sqlalchemy.orm import relationship
 import models
+from models.base_model import BaseModel, Base
 from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade="delete")
+    else:
+        name = ""
 
-    if environ['HBNB_TYPE_STORAGE'] != 'db':
         @property
         def cities(self):
-            '''fileStorage instead of DbStorage between State and City
-            '''
-            cities = models.storage.all(City)
+            city = models.storage.all(City)
             relation = []
-
-            for value in cities:
-                if city.state_id == self.id:
-                    relation.append(value)
+            for key in city.values():
+                if key.states.id == self.id:
+                    relation.append(key)
             return relation
