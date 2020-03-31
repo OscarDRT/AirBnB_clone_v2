80e5aeedf2ccd834d7e3de88d4f18a953eeefa48
Oscar Riaño Tapias <oscarnetworkingpro@gmail.com>
2020-03-31 16:42:48 -0500

    update review

--- models/review.py
+++ models/review.py
@@ -16,7 +16,7 @@ class Review(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         __tablename__ = "reviews"
         text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
 
     place_id = ""
     user_id = ""
