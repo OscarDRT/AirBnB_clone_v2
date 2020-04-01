e523f66c893e6027691a7509ff4988c66b063e08
Oscar Riaño Tapias <oscarnetworkingpro@gmail.com>
2020-03-31 23:27:28 -0500

    Update task 10

--- models/place.py
+++ models/place.py
@@ -26,21 +26,33 @@ class Place(BaseModel, Base):
     """
 
     __tablename__ = 'places'
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
+
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        amenity_ids = []
         reviews = relationship("Review", backref="place",
                                cascade="all, delete")
-
+        amenities = relationship("Amenity",
+                                 secondary=place_amenity,
+                                 back_populates="place_amenities",
+                                 viewonly=False)
     else:
         @property
         def reviews(self):
@@ -50,3 +62,17 @@ class Place(BaseModel, Base):
                 if key.place.id == self.id:
                     relation.append(key)
             return relation
+
+        @property
+        def amenities(self):
+            place = models.storage.all()
+            for key in place:
+                relation = []
+                if key.place_id == self.id and key.__class__.__name__ == "Amenity":
+                    relation.append(key)
+            return relation
+
+        @amenities.setter
+        def amenities(self, obj):
+            if obj.__class__.__name__ == "Amenity":
+                self.amenity_ids.append(obj.amenities.id)
