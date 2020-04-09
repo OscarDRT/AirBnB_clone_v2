47e20e9acd4241d7e2457c548863727252302e00
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-09 16:50:58 -0500

    Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,55 @@
 #!/usr/bin/python3
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+from fabric.api import local, runs_once, run
+from os import stat, path
+from datetime import datetime
+
+
+@runs_once
+def do_pack():
+    try:
+        stat("versions")
+    except:
+        local("mkdir versions")
+
+    try:
+        time = datetime.now().strftime("%Y%m%d%H%M%S")
+        name = "versions/web_static_{}.tgz".format(time)
+        print("Packing web_static to versions/{}".format(name))
+        local("tar -cvzf {} web_static".format(name))
+        file_size = path.getsize(name)
+        print("web_static packed: versions/{} -> \
+            {}Bytes".format(name, file_size))
+        return name
+    except:
+        return None
+
+env.hosts = ["35.227.22.206", "34.235.89.220"]
+
+
+def do_deploy(archive_path):
+    try:
+        stat(archive_path)
+    except:
+        return False
+    try:
+        name = archive_path.split("/")
+        name = name[1].split(".")
+        name = name[0]
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{}.tgz -C \
+            /data/web_static/releases/{}/".format(name, name))
+        run("rm /tmp/{}.tgz".format(name))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current".format(name))
+        print("New version deployed!")
+        return True
+    except:
+        return False
 
 
 def deploy():
