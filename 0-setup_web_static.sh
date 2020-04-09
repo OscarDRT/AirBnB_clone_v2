45c10c711263f4c52eccc81b9707482cb9a6c98e
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-09 15:35:40 -0500

    Bash script that sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y 
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "I am here!!!" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -Rh ubuntu:ubuntu /data/
+sed -i "45i location /hbnb_static{\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+service nginx restart
