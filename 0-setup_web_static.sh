bec9e34a4d1151001e6e1898b013687392ee07eb
OscarDRT <55116114+OscarDRT@users.noreply.github.com>
2020-04-09 16:02:28 -0500

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-#hola
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "I am here!!!" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -Rh ubuntu:ubuntu /data/
-sed -i "45i location /hbnb_static{\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-service nginx restart
+
