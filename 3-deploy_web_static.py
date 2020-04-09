97c787c57237b1b6f6be0816bbfb447cf5503c83
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-09 16:24:32 -0500

    Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+def deploy():
+    file_path = do_pack()
+    if file_path is None:
+        return False
+    return do_deploy(file_path)
