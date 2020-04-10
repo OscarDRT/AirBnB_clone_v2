529c02577fa3023fcba9705017034eaa54af3990
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-10 17:23:28 -0500

    Fabric script that deletes out-of-date archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,18 +8,7 @@ env.hosts = ["35.227.22.206", "34.235.89.220"]
 
 def do_clean(number=0):
     number = int(number)
-    if number < 0:
-        return
-    if number == 0 or number == 1:
-        number = 1
-    loc = local("ls -t versions/", capture=True)
-    loc = str(loc)
-    if loc == "":
-        return
-    loc = loc.split("\n")
-    n = len(loc)
-    for i in range(number, n):
-        local("rm versions/{}".format(loc[i]))
-        name = loc[i].split(".")
-        name = name[0]
-        run("rm -rf /data/web_static/releases/{}/".format(name))
+    local("ls -d -1tr versions/* | tail -n +{} | \
+          xargs -d '\n' rm -f --".format(2 if number < 1 else number + 1))
+    run("ls -d -1tr /data/web_static/releases/* | tail -n +{} | \
+          xargs -d '\n' rm -rf --".format(2 if number < 1 else number + 1))
